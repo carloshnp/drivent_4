cb263c6046e1806f2a61eeb098dd26ae07ec1e17
carloshnp <carloshnpwork@gmail.com>
2023-02-12 15:15:50 -0300

    feat: bookings get and post controllers

---
+++ src/controllers/bookings-controller.ts
@@ -0,0 +1,24 @@
+import { AuthenticatedRequest } from "@/middlewares";
+import { Response } from "express";
+import httpStatus from "http-status";
+
+export async function getBookings(req: AuthenticatedRequest, res: Response) {
+  const { userId } = req;
+  try {
+    const booking = await bookingService.getBooking(userId);
+    return res.status(httpStatus.OK).send(booking);
+  } catch (err) {
+    return res.sendStatus(httpStatus.NOT_FOUND);
+  }
+}
+
+export async function postBookings(req: AuthenticatedRequest, res: Response) {
+  const { userId } = req;
+  const { roomId } = req.body;
+  try {
+    const booking = await bookingService.postBooking(userId, roomId);
+    return res.status(httpStatus.OK).send(booking);
+  } catch (err) {
+    return res.sendStatus(httpStatus.NOT_FOUND);
+  }
+}

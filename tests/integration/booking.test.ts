60710bdf13fc95f54f9f66fb38ea6a9881b3dd62
carloshnp <carloshnpwork@gmail.com>
2023-02-13 11:33:14 -0300

    feat: 404 GET /booking test

--- tests/integration/booking.test.ts
+++ tests/integration/booking.test.ts
@@ -170,7 +170,34 @@ describe("POST /booking", () => {
 });
 
 describe("GET /booking", () => {
+  it("should respond with status 401 if no token", async () => {
+    const res = await server.get("/booking");
+    expect(res.status).toBe(httpStatus.UNAUTHORIZED);
+  });
+
+  it("should respond with status 401 if token is invalid", async () => {
+    const token = faker.lorem.word();
+    const res = await server.get("/booking").set("Authorization", `Bearer ${token}`);
+    expect(res.status).toBe(httpStatus.UNAUTHORIZED);
+  });
+
+  it("should respond with status 401 if no session", async () => {
+    const user = await createUser();
+    const token = jwt.sign({ userId: user.id }, process.env.JWT_SECRET);
+    const res = await server.get("/booking").set("Authorization", `Bearer ${token}`);
+    expect(res.status).toBe(httpStatus.UNAUTHORIZED);
+  });
+
   describe("when token is valid", () => {
+    it("should respond with status 404 if no booking", async () => {
+      const user = await createUser();
+      const token = await generateValidToken(user);
+      const hotel = await createHotel();
+      const room = await createRoomWithHotelId(hotel.id);
+      const res = await server.get("/booking").set("Authorization", `Bearer ${token}`);
+      expect(res.status).toBe(httpStatus.NOT_FOUND);
+    });
+
     it("should respond with status 200 when booking is valid", async () => {
       const user = await createUser();
       const token = await generateValidToken(user);
